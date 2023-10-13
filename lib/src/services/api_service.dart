8774b5ac2384587f2993a868f5f169cef51b3da1
Vladislavs Mihailovs <vladislav.mihailov@bbit.tech>
2023-10-13 16:53:51 +0300

    added getInvoiceDataForHomepage

--- lib/src/services/api_service.dart
+++ lib/src/services/api_service.dart
@@ -1,3 +1,4 @@
+import 'package:hmguru/src/models/InvoiceInfo.dart';
 import 'package:hmguru/src/models/MyLeaseholdVM.dart';
 import 'package:hmguru/src/services/preference_service.dart';
 import 'package:http/http.dart' as http;
@@ -27,6 +28,29 @@ class ApiService {
       final jsonBody = jsonDecode(response.body);
       final myLeaseholdVM = MyLeaseholdVM.fromJson(jsonBody);
       _preferenceservice.saveLeaseholdData(myLeaseholdVM);
+    }
+  }
+
+  Future<MyLeaseholdVM?> getInvoiceDataForHomepage() async {
+    final String? jwtToken = await _preferenceservice.loadJwtToken();
+
+    final headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $jwtToken',
+    };
+
+    final response = await http.get(
+      Uri.parse(
+          'http://10.0.2.2:13016/api/my/invoice-info'), // Change the API URL to match your server
+      headers: headers,
+    );
+
+    if (response.statusCode == 200) {
+      print(response.body);
+      final jsonBody = jsonDecode(response.body);
+      final myInvoiceInfo = InvoiceInfo.fromJson(jsonBody);
+      _preferenceservice.saveInvoiceInfo(myInvoiceInfo);
     }
   }
 }
