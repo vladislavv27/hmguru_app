bea55707f918d7ab91fbab83f4287ff669120be2
Vladislavs Mihailovs <vladislav.mihailov@bbit.tech>
2023-10-17 13:49:29 +0300

    page redesigned invoice_detail, invoice_list model rewrited

--- lib/src/models/invoice_list.dart
+++ lib/src/models/invoice_list.dart
@@ -1,55 +1,99 @@
-class InvoiceListVM {
-  final String serviceTitle;
-  final String priceForService;
-  final String priceTaxTotal;
-  final String debtForService;
-  final String priceForServiceTotal;
-  final String payedForService;
-  final String penaltyForService;
-  final String penaltyForServiceTotal;
-  final String debtRecalculationValue;
-  final String rateFormula;
+class InvoiceList {
+  final String invoiceUID;
+  final String payed;
+  final String priceRecalculationValueTotal;
+  final String sumTotal;
+  final String toPayForPeriod;
+  final String debt;
+  final String penalty;
+  final String paymentSum;
+  final String paymentAdjustmentSum;
+  final String previousBalance;
+  final DateTime period;
+  final String leaseholdId;
+  final String id;
+  final DateTime created;
+  final String createdById;
+  final dynamic createdBy;
+  final DateTime modified;
+  final String modifiedById;
+  final dynamic modifiedBy;
+  final bool isDeleted;
+  final String rowVersion;
 
-  InvoiceListVM({
-    required this.serviceTitle,
-    required this.priceForService,
-    required this.priceTaxTotal,
-    required this.debtForService,
-    required this.priceForServiceTotal,
-    required this.payedForService,
-    required this.penaltyForService,
-    required this.penaltyForServiceTotal,
-    required this.debtRecalculationValue,
-    required this.rateFormula,
+  InvoiceList({
+    required this.invoiceUID,
+    required this.payed,
+    required this.priceRecalculationValueTotal,
+    required this.sumTotal,
+    required this.toPayForPeriod,
+    required this.debt,
+    required this.penalty,
+    required this.paymentSum,
+    required this.paymentAdjustmentSum,
+    required this.previousBalance,
+    required this.period,
+    required this.leaseholdId,
+    required this.id,
+    required this.created,
+    required this.createdById,
+    required this.modified,
+    required this.modifiedById,
+    required this.isDeleted,
+    required this.rowVersion,
+    required this.createdBy,
+    required this.modifiedBy,
   });
 
-  factory InvoiceListVM.fromJson(Map<String, dynamic> json) {
-    return InvoiceListVM(
-      serviceTitle: json['serviceTitle'] ?? '',
-      priceForService: json['priceForService'] ?? '',
-      priceTaxTotal: json['priceTaxTotal'] ?? '',
-      debtForService: json['debtForService'] ?? '',
-      priceForServiceTotal: json['priceForServiceTotal'] ?? '',
-      payedForService: json['payedForService'] ?? '',
-      penaltyForService: json['penaltyForService'] ?? '',
-      penaltyForServiceTotal: json['penaltyForServiceTotal'] ?? '',
-      debtRecalculationValue: json['debtRecalculationValue'] ?? '',
-      rateFormula: json['rateFormula'] ?? '',
+  factory InvoiceList.fromJson(Map<String, dynamic> json) {
+    return InvoiceList(
+      invoiceUID: json['invoiceUID'] ?? '',
+      payed: json['payed'] ?? '',
+      priceRecalculationValueTotal: json['priceRecalculationValueTotal'] ?? '',
+      sumTotal: json['sumTotal'] ?? '',
+      toPayForPeriod: json['toPayForPeriod'] ?? '',
+      debt: json['debt'] ?? '',
+      penalty: json['penalty'] ?? '',
+      paymentSum: json['paymentSum'] ?? '',
+      paymentAdjustmentSum: json['paymentAdjustmentSum'] ?? '',
+      previousBalance: json['previousBalance'] ?? '',
+      period: DateTime.parse(json['period']),
+      leaseholdId: json['leaseholdId'] ?? '',
+      id: json['id'] ?? '',
+      created: DateTime.parse(json['created']),
+      createdById: json['createdById'] ?? '',
+      modified: DateTime.parse(json['modified']),
+      modifiedById: json['modifiedById'] ?? '',
+      isDeleted: json['isDeleted'] ?? false,
+      rowVersion: json['rowVersion'] ?? '',
+      createdBy: json['createdBy'],
+      modifiedBy: json['modifiedBy'],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'serviceTitle': serviceTitle,
-      'priceForService': priceForService,
-      'priceTaxTotal': priceTaxTotal,
-      'debtForService': debtForService,
-      'priceForServiceTotal': priceForServiceTotal,
-      'payedForService': payedForService,
-      'penaltyForService': penaltyForService,
-      'penaltyForServiceTotal': penaltyForServiceTotal,
-      'debtRecalculationValue': debtRecalculationValue,
-      'rateFormula': rateFormula,
+      'invoiceUID': invoiceUID,
+      'payed': payed,
+      'priceRecalculationValueTotal': priceRecalculationValueTotal,
+      'sumTotal': sumTotal,
+      'toPayForPeriod': toPayForPeriod,
+      'debt': debt,
+      'penalty': penalty,
+      'paymentSum': paymentSum,
+      'paymentAdjustmentSum': paymentAdjustmentSum,
+      'previousBalance': previousBalance,
+      'period': period.toIso8601String(),
+      'leaseholdId': leaseholdId,
+      'id': id,
+      'created': created.toIso8601String(),
+      'createdById': createdById,
+      'modified': modified.toIso8601String(),
+      'modifiedById': modifiedById,
+      'isDeleted': isDeleted,
+      'rowVersion': rowVersion,
+      'createdBy': createdBy,
+      'modifiedBy': modifiedBy,
     };
   }
 }
--- lib/src/pages/invoice_details.dart
+++ lib/src/pages/invoice_details.dart
@@ -13,63 +13,95 @@ class InvoiceDetailPage extends StatefulWidget {
 class _InvoiceDetailPageState extends State<InvoiceDetailPage> {
   double _scale = 1.0;
   double _previousScale = 1.0;
-
   @override
   Widget build(BuildContext context) {
+    double totalPrice = calculateTotalPrice(); // Calculate the total price
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Invoice Detail'),
         backgroundColor: Theme.of(context).primaryColor,
       ),
-      body: GestureDetector(
-        onScaleStart: (details) {
-          _previousScale = _scale;
-        },
-        onScaleUpdate: (details) {
-          setState(() {
-            _scale = _previousScale * details.scale;
-          });
-        },
-        child: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Transform.scale(
-              scale: _scale,
-              child: DataTable(
-                columns: <DataColumn>[
-                  DataColumn(label: Text('SERVICE')),
-                  DataColumn(label: Text('PRICE')),
-                  DataColumn(label: Text('TAX')),
-                  DataColumn(label: Text('DEBT')),
-                  DataColumn(label: Text('TO PAY')),
-                  DataColumn(label: Text('PAID')),
-                  DataColumn(label: Text('PENALTY FOR PERIOD')),
-                  DataColumn(label: Text('PENALTY')),
-                  DataColumn(label: Text('RECALCULATION')),
-                  DataColumn(label: Text('FORMULA')),
-                ],
-                rows: widget.data.map((rowData) {
-                  return DataRow(
-                    cells: [
-                      DataCell(Text(rowData.serviceTitle)),
-                      DataCell(Text(rowData.priceForService)),
-                      DataCell(Text(rowData.priceTaxTotal)),
-                      DataCell(Text(rowData.debtForService)),
-                      DataCell(Text(rowData.priceForServiceTotal)),
-                      DataCell(Text(rowData.payedForService)),
-                      DataCell(Text(rowData.penaltyForService)),
-                      DataCell(Text(rowData.penaltyForServiceTotal)),
-                      DataCell(Text(rowData.debtRecalculationValue)),
-                      DataCell(Text(rowData.rateFormula)),
-                    ],
-                  );
-                }).toList(),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Invoice: ${widget.data.isNotEmpty ? widget.data[0].invoiceUID : ""}',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'To pay: ${totalPrice.toStringAsFixed(2)}',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Transform.scale(
+                scale: _scale,
+                child: DataTable(
+                  columns: <DataColumn>[
+                    DataColumn(label: Text('SERVICE')),
+                    DataColumn(label: Text('PRICE')),
+                    DataColumn(label: Text('TAX')),
+                    DataColumn(label: Text('DEBT')),
+                    DataColumn(label: Text('TO PAY')),
+                    DataColumn(label: Text('PAID')),
+                    DataColumn(label: Text('PENALTY FOR PERIOD')),
+                    DataColumn(label: Text('PENALTY')),
+                    DataColumn(label: Text('RECALCULATION')),
+                    DataColumn(label: Text('FORMULA')),
+                  ],
+                  rows: widget.data.map((rowData) {
+                    return DataRow(
+                      cells: [
+                        DataCell(Text(rowData.serviceTitle)),
+                        DataCell(Text(rowData.priceForService)),
+                        DataCell(Text(rowData.priceTaxTotal)),
+                        DataCell(Text(rowData.debtForService)),
+                        DataCell(Text(rowData.priceForServiceTotal)),
+                        DataCell(Text(rowData.payedForService)),
+                        DataCell(Text(rowData.penaltyForService)),
+                        DataCell(Text(rowData.penaltyForServiceTotal)),
+                        DataCell(Text(rowData.debtRecalculationValue)),
+                        DataCell(Text(rowData.rateFormula)),
+                      ],
+                    );
+                  }).toList(),
+                ),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
+  }
+
+  double calculateTotalPrice() {
+    double total = 0.0;
+    for (var rowData in widget.data) {
+      total += double.parse(rowData.priceForServiceTotal);
+    }
+    return total;
   }
 }
