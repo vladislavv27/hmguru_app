9ae7fe3d94a7d8ef5822665cf1849916f0f15e59
Vladislavs Mihailovs <vladislav.mihailov@bbit.tech>
2023-10-31 15:16:45 +0200

    Update Flutter code for internationalization

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:hmguru/l10n/global_localizations.dart';
-import 'package:hmguru/src/models/app_colors.dart';
-import 'package:hmguru/src/pages/login_view.dart';
+import 'package:hmguru/src/controllers/material_app_with_locale.dart';
 
 void main() async {
   await dotenv.load();
@@ -14,7 +11,6 @@ class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 
-  // Define a static method to allow changing the app's locale
   static void setLocale(BuildContext context, Locale newLocale) {
     _MyAppState state = context.findAncestorStateOfType<_MyAppState>()!;
     state.setLocale(newLocale);
@@ -22,9 +18,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Locale _appLocale = Locale('en'); // Default locale is English
+  Locale _appLocale = Locale('en');
 
-  // Method to update the app's locale
   void setLocale(Locale newLocale) {
     setState(() {
       _appLocale = newLocale;
@@ -34,35 +29,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialAppWithLocale(
-      appLocale: _appLocale, // Pass the app's locale to MaterialAppWithLocale
-    );
-  }
-}
-
-class MaterialAppWithLocale extends StatelessWidget {
-  final Locale appLocale;
-
-  MaterialAppWithLocale({required this.appLocale});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primaryColor: AppColors.primaryColor,
-      ),
-      home: LoginView(),
-      localizationsDelegates: [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: [
-        const Locale('en'),
-        const Locale('lv'),
-        const Locale('ru'),
-      ],
-      locale: appLocale, // Use the selected locale
+      appLocale: _appLocale,
     );
   }
 }
---
+++ lib/src/controllers/material_app_with_locale.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:hmguru/l10n/global_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:hmguru/src/models/app_colors.dart';
+import 'package:hmguru/src/pages/login_view.dart';
+
+class MaterialAppWithLocale extends StatelessWidget {
+  final Locale appLocale;
+
+  MaterialAppWithLocale({required this.appLocale});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: AppColors.primaryColor,
+      ),
+      home: LoginView(),
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en'),
+        const Locale('lv'),
+        const Locale('ru'),
+      ],
+      locale: appLocale,
+    );
+  }
+}
